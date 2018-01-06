a186e5eb4e79b9f110037e1c06fef0f9e3cc25b9
mGuv <mattmcg.gamedev@gmail.com>
2018-01-06 18:55:20 +0000

    Initial commit with project

---
+++ src/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Advent2017
+{
+    /// <summary>
+    /// Initial program script for when executable is ran.
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Initial function/entry point when the application it executed.
+        /// </summary>
+        /// <param name="args">The arguments passed to the executable when it was executed.</param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press any key to quit:");
+            Console.ReadKey();
+        }
+    }
+}
